bab3c10eced5ffd32262e5f911840fed0ba39e40
Phạm Phan Nhật Khánh <phamphannhatkhanh7520@gmail.com>
2025-02-24 21:55:49 +0700

    Complete ex4

--- tests/lesson-2/ex4.js
+++ tests/lesson-2/ex4.js
@@ -1,5 +1,9 @@
-let myHeight;
-let goodWeight = (myHeight * 9) / 10;
-let maxWeight;
-let minWeight;
+let height = 165;
+let heightConvert = height % 100;
 
+let maxWeight = heightConvert;
+let minWeight = (heightConvert * 8) / 10;
+let perfectWeight = (heightConvert * 9) / 10;
+
+console.log('Best weight: ' + perfectWeight + ' Max weight by Kg: ' +
+    maxWeight + ' Min weight by Kg: ' + minWeight + 'kg');
